9e7098dbfde202edf9cd6a2f2df60ceedbfb033d
rajathshttgr <rathshettigar.uy@gmail.com>
2025-03-18 00:55:23 +0530

    Minor changes - test commit

--- frontend/src/app/dashboard/donations/page.tsx
+++ frontend/src/app/dashboard/donations/page.tsx
@@ -5,6 +5,7 @@ import underconstruction from "../../../assets/backgrounds/underConstruction.jpg
 const page = () => {
   return (
     <div className="flex justify-center">
+      <p>This Page is under underConstruction, come back after some days.</p>
       <Image
         src={underconstruction}
         className="m-2 h-96 w-96"
