f7cceb93afd4f97c598d687fa3f8053cc591fca7
Rajath Shettigar <rajathshettigar.uy@gmail.com>
2025-04-12 21:57:10 +0530

    minor bug fix for deployment #3

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ["fundfable.onrender.com"], // if using next/image
+  },
+  reactStrictMode: true,
 };
 
 export default nextConfig;
--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,4 +1,4 @@
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
-const BASE_URL_USER = process.env.NEXT_PUBLIC_BASE_URL_USER;
+const BASE_URL_USER = "http://localhost:3001";
 
 export { BASE_URL, BASE_URL_USER };
